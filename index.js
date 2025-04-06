04cf7d8dfca21075387c9ebceae11012613588ba
centerpedicure <centerpedicure@gmail.com>
2025-04-06 16:16:57 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const allowedOrigin = "центр-педикюра-1.рф";
+const allowedOrigin = "https://xn----1-6cdtebzszxkgu1d7h.xn--p1ai/";
 
 app.use(
   cors({
